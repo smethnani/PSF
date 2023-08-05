509b083bef0f8d586ffd9baced3b2ec4a9346216
smethnani <smethnani@m2.local>
2023-08-05 14:28:32 +0400

    load optim state

--- create_samples_for_reflow.py
+++ create_samples_for_reflow.py
@@ -11,18 +11,18 @@ from utils.visualize import *
 from model.pvcnn_generation import PVCNN2Base
 import torch.distributed as dist
 from datasets.shapenet_data_pc import ShapeNet15kPointClouds
-from pytorch3d.loss import chamfer_distance
-from ot_loss import SlicedWassersteinDist, SphericalSlicedWassersteinDist
+
 
 class Flowmodel:
-    def __init__(self):
+    def __init__(self, opt):
+        self.num_timesteps = opt.time_num
         return
 
     def p_mean(self, denoise_fn, data, t):
 
         model_output = denoise_fn(data, t)
 
-        model_mean = data + model_output * 1 / 1.
+        model_mean = data + model_output * 1. / self.num_timesteps
 
         return model_mean
 
@@ -38,7 +38,7 @@ class Flowmodel:
         return model_mean
 
 
-    def p_sample_loop(self, denoise_fn, shape, device, noise=None,
+    def p_sample_loop(self, denoise_fn, shape, device,
                       noise_fn=torch.randn, clip_denoised=True, keep_running=False):
         """
         Generate samples
@@ -47,10 +47,8 @@ class Flowmodel:
         """
 
         assert isinstance(shape, (tuple, list))
-        img_t = noise
-        if img_t is None:
-            img_t = noise_fn(size=shape, dtype=torch.float, device=device)
-        for t in range(1):
+        img_t = noise_fn(size=shape, dtype=torch.float, device=device)
+        for t in range(self.num_timesteps):
             t_ = torch.empty(shape[0], dtype=torch.int64, device=device).fill_(t)
             img_t = self.p_sample(denoise_fn=denoise_fn, data=img_t,t=t_, noise_fn=noise_fn,
                                   clip_denoised=clip_denoised, return_pred_xstart=False)
@@ -58,7 +56,7 @@ class Flowmodel:
         assert img_t.shape == shape
         return img_t
 
-    def p_sample_loop_trajectory(self, denoise_fn, shape, device, freq, noise=None,
+    def p_sample_loop_trajectory(self, denoise_fn, shape, device, freq,
                                  noise_fn=torch.randn,clip_denoised=True, keep_running=False):
         """
         Generate samples, returning intermediate images
@@ -69,13 +67,11 @@ class Flowmodel:
         """
         assert isinstance(shape, (tuple, list))
 
-        total_steps =  1 #self.num_timesteps if not keep_running else len(self.betas)
+        total_steps =  self.num_timesteps if not keep_running else len(self.betas)
 
-        img_t = noise
-        if img_t is None:
-            img_t = noise_fn(size=shape, dtype=torch.float, device=device)
+        img_t = noise_fn(size=shape, dtype=torch.float, device=device)
         imgs = [img_t]
-        for t in range(1):
+        for t in range(self.num_timesteps):
 
             t_ = torch.empty(shape[0], dtype=torch.int64, device=device).fill_(t)
             img_t = self.p_sample(denoise_fn=denoise_fn, data=img_t, t=t_, noise_fn=noise_fn,
@@ -84,40 +80,31 @@ class Flowmodel:
             if t % freq == 0 or t == total_steps-1:
                 imgs.append(img_t)
 
+        assert imgs[-1].shape == shape
         return imgs
 
     @torch.no_grad()
-    def sample_pairs(self, x0 = None, x1 = None):
+    def sample_pairs(self, data, x0 = None):
         if x0 is None:
             x0 = torch.randn_like(data)
-        data = x1
+
         z0 = x0
-        t= torch.zeros((data.shape[0], 1, 1)).to(data.device)
-        inter_data = z0
+        t= torch.rand((data.shape[0], 1, 1)).to(data.device)
+        inter_data = t * data + (1.-t) * z0
         target = data - z0
         return inter_data, t * 999, target
 
-    def p_losses(self, denoise_fn, data_start, t, loss_type):
+    def p_losses(self, denoise_fn, data_start, t, noise=None):
         """
         Training loss calculation
         """
-        x0, x1 = data_start
-        data_start = x1
-        inter_data, t, target = self.sample_pairs(x0 = x0, x1 = x1)
+        inter_data, t, target = self.sample_pairs(data_start)
         t = t.squeeze()
         data_t = inter_data
         eps_recon = denoise_fn(data_t, t)
-        P = x1.permute(0, 2, 1)
-        Q = (x0 + eps_recon).permute(0, 2, 1)
-        if loss_type == "chamfer":
-            losses, _  = chamfer_distance(P, Q)
-            return losses
-        elif loss_type == "swd":
-            losses = SlicedWassersteinDist()(P, Q)
-            return losses
-        else:
-            losses = SphericalSlicedWassersteinDist()(P, Q)
-            return losses
+        losses = ((target - eps_recon)**2).mean(dim=list(range(1, len(data_start.shape))))
+
+        return losses
 
 class PVCNN2(PVCNN2Base):
     sa_blocks = [
@@ -145,7 +132,7 @@ class PVCNN2(PVCNN2Base):
 class Model(nn.Module):
     def __init__(self, args, betas, loss_type: str, model_mean_type: str, model_var_type:str):
         super(Model, self).__init__()
-        self.flow = Flowmodel()
+        self.flow = Flowmodel(args)
 
         self.model = PVCNN2(num_classes=args.nc, embed_dim=args.embed_dim, use_att=args.attention,
                             dropout=args.dropout, extra_feature_channels=0)
@@ -153,33 +140,37 @@ class Model(nn.Module):
 
     def _denoise(self, data, t):
         B, D,N= data.shape
+        assert data.dtype == torch.float
+        #assert t.shape == torch.Size([B]) and t.dtype == torch.int64
+
         out = self.model(data, t)
 
+        assert out.shape == torch.Size([B, D, N])
         return out
 
-    def get_loss_iter(self, data, loss_type, noises=None):
-        x0, x1 = data
-        data = x0
+    def get_loss_iter(self, data, noises=None):
+
         B, D, N = data.shape
         t = torch.randint(0, 1000, size=(B,), device=data.device)
 
-        # if noises is not None:
-        #     noises[t!=0] = torch.randn((t!=0).sum(), *noises.shape[1:]).to(noises)
-        data = [x0, x1]
+        if noises is not None:
+            noises[t!=0] = torch.randn((t!=0).sum(), *noises.shape[1:]).to(noises)
+
         losses = self.flow.p_losses(
-            denoise_fn=self._denoise, data_start=data, t=t, loss_type=loss_type)
+            denoise_fn=self._denoise, data_start=data, t=t, noise=noises)
+        assert losses.shape == t.shape == torch.Size([B])
         return losses
 
-    def gen_samples(self, shape, device, noise=None, noise_fn=torch.randn,
+    def gen_samples(self, shape, device, noise_fn=torch.randn,
                     clip_denoised=True,
                     keep_running=False):
-        return self.flow.p_sample_loop(self._denoise, shape=shape, device=device, noise=noise, noise_fn=noise_fn,
+        return self.flow.p_sample_loop(self._denoise, shape=shape, device=device, noise_fn=noise_fn,
                                             clip_denoised=clip_denoised,
                                             keep_running=keep_running)
 
-    def gen_sample_traj(self, shape, device, freq, noise=None, noise_fn=torch.randn,
+    def gen_sample_traj(self, shape, device, freq, noise_fn=torch.randn,
                     clip_denoised=True,keep_running=False):
-        return self.flow.p_sample_loop_trajectory(self._denoise, shape=shape, device=device, noise=noise, noise_fn=noise_fn, freq=freq,
+        return self.flow.p_sample_loop_trajectory(self._denoise, shape=shape, device=device, noise_fn=noise_fn, freq=freq,
                                                        clip_denoised=clip_denoised,
                                                        keep_running=keep_running)
 
@@ -216,14 +207,13 @@ def get_betas(schedule_type, b_start, b_end, time_num):
     return betas
 
 
-def get_dataset(dataroot, npoints, category, reflow_sample_path,):
+def get_dataset(dataroot, npoints,category):
     tr_dataset = ShapeNet15kPointClouds(root_dir=dataroot,
         categories=[category], split='train',
         tr_sample_size=npoints,
         te_sample_size=npoints,
         scale=1.,
-        reflow = True,
-        reflow_sample_path=reflow_sample_path,
+        reflow = False,
         normalize_per_shape=False,
         normalize_std_per_axis=False,
         random_subsample=True)
@@ -232,8 +222,7 @@ def get_dataset(dataroot, npoints, category, reflow_sample_path,):
         tr_sample_size=npoints,
         te_sample_size=npoints,
         scale=1.,
-        reflow = True,
-        reflow_sample_path=reflow_sample_path,
+        reflow = False,
         normalize_per_shape=False,
         normalize_std_per_axis=False,
         all_points_mean=tr_dataset.all_points_mean,
@@ -274,9 +263,9 @@ def get_dataloader(opt, train_dataset, test_dataset=None):
     return train_dataloader, test_dataloader, train_sampler, test_sampler
 
 
-def train(gpu, opt, output_dir, wandb_run=None):
+def train(gpu, opt, output_dir, noises_init, wandb_run=None):
     if wandb_run is None:
-        wandb_run = wandb.init(group='train-distill', config=opt, project='shapes-exp')
+        wandb_run = wandb.init(group='train-flow', config=opt, project='shapes-exp')
     set_seed(opt)
     logger = setup_logging(output_dir)
     if opt.distribution_type == 'multi':
@@ -304,7 +293,7 @@ def train(gpu, opt, output_dir, wandb_run=None):
 
 
     ''' data '''
-    train_dataset, _ = get_dataset(opt.dataroot, opt.npoints, opt.category, opt.reflow_sample_path)
+    train_dataset, _ = get_dataset(opt.dataroot, opt.npoints, opt.category)
     dataloader, _, train_sampler, _ = get_dataloader(opt, train_dataset, None)
 
 
@@ -348,9 +337,7 @@ def train(gpu, opt, output_dir, wandb_run=None):
         ckpt = torch.load(opt.model)
         model.load_state_dict(ckpt['model_state'])
         optimizer.load_state_dict(ckpt['optimizer_state'])
-
-    if opt.model != '':
-        start_epoch = 0
+        start_epoch = ckpt['epoch'] + 1
     else:
         start_epoch = 0
 
@@ -358,30 +345,26 @@ def train(gpu, opt, output_dir, wandb_run=None):
         return torch.randn(num_chain, *x.shape[1:], device=x.device)
 
 
-
     for epoch in range(start_epoch, opt.niter):
 
         if opt.distribution_type == 'multi':
             train_sampler.set_epoch(epoch)
 
-        for i, data in enumerate(dataloader):
-            x0 = data['train_points0']
-            x1 = data['train_points1']
-            # noises_batch = noises_init[data['idx']].transpose(1,2)
 
-            '''
-            train diffusion
-            '''
+        for i, data in enumerate(dataloader):
+            
+            x = data['train_points'].transpose(1,2)
+            
+            noises_batch = noises_init[data['idx']].transpose(1,2)
 
             if opt.distribution_type == 'multi' or (opt.distribution_type is None and gpu is not None):
-                x0 = x0.cuda(gpu)
-                x1 = x1.cuda(gpu)
-                # noises_batch = noises_batch.cuda(gpu)
+                x = x.cuda(gpu)
+                noises_batch = noises_batch.cuda(gpu)
             elif opt.distribution_type == 'single':
                 x = x.cuda()
-                # noises_batch = noises_batch.cuda()
-            x = [x0, x1]
-            loss = model.get_loss_iter(x, opt.loss_type).mean()
+                noises_batch = noises_batch.cuda()
+            
+            loss = model.get_loss_iter(x, noises_batch).mean()
 
             optimizer.zero_grad()
             loss.backward()
@@ -393,25 +376,30 @@ def train(gpu, opt, output_dir, wandb_run=None):
 
 
             if i % opt.print_freq == 0 and should_diag:
-
-                logger.info('[{:>3d}/{:>3d}][{:>3d}/{:>3d}]    loss: {:>10.4f},    '
-                            #'netpNorm: {:>10.2f},   netgradNorm: {:>10.2f}     '
+                logger.info('[{:>3d}/{:>3d}][{:>3d}/{:>3d}]    loss: {:>10.4f},'
                              .format(
-                        epoch, opt.niter, i, len(dataloader),loss.item(),
-                    #netpNorm, netgradNorm,
+                        epoch, opt.niter, i, len(dataloader), loss.item(), 
                         ))
+                scheduler_state = ', '.join(['{} {}'.format(item, value) for item, value in lr_scheduler.state_dict().items()])
+                logger.info(scheduler_state)
+                wandb_run.log({
+                    'epoch': epoch,
+                    'batch': i,
+                    'loss': loss.item()
+                })
 
 
         lr_scheduler.step()
+        
         if epoch % opt.vizIter == 0 and should_diag:
             logger.info('Generation: eval')
 
             model.eval()
-            x = x1
+            x = x
             with torch.no_grad():
-                bs = 10 if x0.shape[0] > 10 else x0.shape[0]
-                x_gen_eval = model.gen_samples(new_x_chain(x0, bs).shape, x.device, noise=x0[:bs], clip_denoised=False)
-                x_gen_list = model.gen_sample_traj(new_x_chain(x0, 1).shape, x.device, noise=x0[:1], freq=40, clip_denoised=False)
+
+                x_gen_eval = model.gen_samples(new_x_chain(x, 25).shape, x.device, clip_denoised=False)
+                x_gen_list = model.gen_sample_traj(new_x_chain(x, 1).shape, x.device, freq=40, clip_denoised=False)
                 x_gen_all = torch.cat(x_gen_list, dim=0)
 
                 gen_stats = [x_gen_eval.mean(), x_gen_eval.std()]
@@ -438,15 +426,15 @@ def train(gpu, opt, output_dir, wandb_run=None):
                     })
 
             visualize_pointcloud_batch('%s/epoch_%03d_samples_eval.png' % (outf_syn, epoch),
-                                       x_gen_eval.transpose(1, 2), None, None,
+                                       x_gen_T, None, None,
                                        None)
 
             visualize_pointcloud_batch('%s/epoch_%03d_samples_eval_all.png' % (outf_syn, epoch),
-                                       x_gen_all.transpose(1, 2), None,
+                                       x_gen_all_T, None,
                                        None,
                                        None)
 
-            visualize_pointcloud_batch('%s/epoch_%03d_x.png' % (outf_syn, epoch), x.transpose(1, 2), None,
+            visualize_pointcloud_batch('%s/epoch_%03d_x.png' % (outf_syn, epoch), x_T, None,
                                        None,
                                        None)
 
@@ -454,27 +442,19 @@ def train(gpu, opt, output_dir, wandb_run=None):
             model.train()
 
 
-
-
         if (epoch + 1) % opt.saveIter == 0:
-
             if should_diag:
-
-
                 save_dict = {
                     'epoch': epoch,
                     'model_state': model.state_dict(),
                     'optimizer_state': optimizer.state_dict()
                 }
-                path = '%s/epoch_%d.pth' % (output_dir, epoch)
-                torch.save(save_dict, path)
+                save_path = '%s/epoch_%d.pth' % (output_dir, epoch)
+                torch.save(save_dict, save_path)
                 if wandb_run is not None:
-                    save_name = "reflow-epoch_%s.pth" % epoch
-                    artifact = wandb.Artifact(save_name, type='model')
-                    artifact.add_file(path)
+                    artifact = wandb.Artifact('flow-epoch_%d.pth' % epoch, type='model')
+                    artifact.add_file(save_path)
                     wandb_run.log_artifact(artifact)
-                torch.save(save_dict, '%s/epoch_%d.pth' % (output_dir, epoch))
-
 
             if opt.distribution_type == 'multi':
                 dist.barrier()
@@ -482,6 +462,7 @@ def train(gpu, opt, output_dir, wandb_run=None):
                 model.load_state_dict(
                     torch.load('%s/epoch_%d.pth' % (output_dir, epoch), map_location=map_location)['model_state'])
 
+    wandb_run.finish()
     dist.destroy_process_group()
 
 def main():
@@ -499,8 +480,8 @@ def main():
     copy_source(__file__, output_dir)
 
     ''' workaround '''
-    # train_dataset, _ = get_dataset(opt.dataroot, opt.npoints, opt.category, opt.reflow_sample_path)
-    # noises_init = torch.randn(len(train_dataset), opt.npoints, opt.nc)
+    train_dataset, _ = get_dataset(opt.dataroot, opt.npoints, opt.category)
+    noises_init = torch.randn(len(train_dataset), opt.npoints, opt.nc)
 
     if opt.dist_url == "env://" and opt.world_size == -1:
         opt.world_size = int(os.environ["WORLD_SIZE"])
@@ -508,23 +489,22 @@ def main():
     if opt.distribution_type == 'multi':
         opt.ngpus_per_node = torch.cuda.device_count()
         opt.world_size = opt.ngpus_per_node * opt.world_size
-        mp.spawn(train, nprocs=opt.ngpus_per_node, args=(opt, output_dir))
+        mp.spawn(train, nprocs=opt.ngpus_per_node, args=(opt, output_dir, noises_init))
     else:
         run = wandb.init(config=opt, project='shapes-exp')
-        train(opt.gpu, opt, output_dir, wandb_run=run)
+        train(opt.gpu, opt, output_dir, noises_init, wandb_run=run)
 
 
 
 def parse_args():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataroot', default='./data/ShapeNetCore.v2.PC15k/')
-    parser.add_argument('--reflow_sample_path', default='DATASET.pth')
-    parser.add_argument('--category', default='chair')
+    parser.add_argument('--dataroot', default='./ShapeNetCore.v2.PC15k/')
+    parser.add_argument('--category', default='car')
 
     parser.add_argument('--bs', type=int, default=32, help='input batch size')
     parser.add_argument('--workers', type=int, default=16, help='workers')
-    parser.add_argument('--niter', type=int, default=10000, help='number of epochs to train for')
+    parser.add_argument('--niter', type=int, default=20000, help='number of epochs to train for')
 
     parser.add_argument('--nc', default=3)
     parser.add_argument('--npoints', default=2048)
@@ -538,12 +518,11 @@ def parse_args():
     parser.add_argument('--attention', default=True)
     parser.add_argument('--dropout', default=0.1)
     parser.add_argument('--embed_dim', type=int, default=64)
-    # parser.add_argument('--loss_type', default='mse')
+    parser.add_argument('--loss_type', default='mse')
     parser.add_argument('--model_mean_type', default='eps')
     parser.add_argument('--model_var_type', default='fixedsmall')
-    parser.add_argument('--loss_type', default='chamfer', choices=['chamfer', 'swd', 'sswd', None])
 
-    parser.add_argument('--lr', type=float, default=2e-5, help='learning rate for E, default=0.0002')
+    parser.add_argument('--lr', type=float, default=2e-4, help='learning rate for E, default=0.0002')
     parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
     parser.add_argument('--decay', type=float, default=0, help='weight decay for EBM')
     parser.add_argument('--grad_clip', type=float, default=None, help='weight decay for EBM')
@@ -570,9 +549,9 @@ def parse_args():
                         help='GPU id to use. None means using all available GPUs.')
 
     '''eval'''
-    parser.add_argument('--saveIter', default=30, help='unit: epoch')
+    parser.add_argument('--saveIter', default=400, help='unit: epoch')
     parser.add_argument('--diagIter', default=100, help='unit: epoch')
-    parser.add_argument('--vizIter', default=10, help='unit: epoch')
+    parser.add_argument('--vizIter', default=200, help='unit: epoch')
     parser.add_argument('--print_freq', default=50, help='unit: iter')
     parser.add_argument('--outdir', default='', help='output directory')
 
